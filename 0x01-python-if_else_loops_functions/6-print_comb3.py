a457bdb9390f44f8e22d556de83108499ae4f606
Valentine Baraka <96381376+vamreala@users.noreply.github.com>
2023-03-11 18:45:49 +0300

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
  #!/usr/bin/python3
-for i in range(100):
-    if int(i / 10) != i % 10 and int(i / 10) < i % 10:
-        print("{}{}".format(int(i / 10), i % 10), end="")
-        if (i != 89):
-            print(", ", end="")
-print("")
+for i in range(10):
+    for j in range(i+1, 10):
+        print("{}, {}".format(i, j), end=", ")
+print("\n")
+
